af25b592ee295526c08d0134a8a63a581dcb7b4e
Mark Haynes <mark.haynes@winnowsolutions.com>
2019-05-19 19:16:01 +0100

    [Trivial] minor bump of version to deploy to pypi

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='rxpy_backpressure',
-    version='0.0.1',
+    version='0.1.0',
     packages=find_packages(),
     description='Simple add ons to be used with RXPY to support back pressure',
     author='Mark Haynes',
