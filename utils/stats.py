b1a7bddfb370884135d6f2e7d9a3dd332a90d7cc
Bogdan Condurache <bogdan.condurache@winnowsollutions.com>
2019-09-12 10:38:41 +0300

    Addition of polling to get analytics for drop back pressure strategy

--- utils/stats.py
+++ utils/stats.py
@@ -1,13 +1,14 @@
 from functools import wraps
 from time import time
+from typing import List
 
 
 class Counter:
     def __init__(self):
         self.__timestamp_last_cached: float = 0
-        self.__total_wait_time_sec: float = 0
         self.__number_processed_events: int = 0
         self.__number_dropped_events: int = 0
+        self.__waited_times: List[float] = []
 
     def time(self, func):
         @wraps(func)
@@ -16,7 +17,7 @@ class Counter:
                 self.__timestamp_last_cached = time()
             else:
                 current_time = time()
-                self.__total_wait_time_sec += current_time
+                self.__waited_times.append(current_time - self.__timestamp_last_cached)
                 self.__timestamp_last_cached = current_time
             return func(*args, **kwargs)
 
@@ -39,3 +40,12 @@ class Counter:
             return func(*args, **kwargs)
 
         return wrapper
+
+    def get_stats(self) -> dict:
+        return {
+            "dropped_events": self.__number_dropped_events,
+            "successful_events": self.__number_processed_events
+            - self.__number_dropped_events,
+            "mean_time": sum(self.__waited_times) / len(self.__waited_times),
+            "max_time": max(self.__waited_times),
+        }
